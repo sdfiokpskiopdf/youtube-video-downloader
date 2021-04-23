3f65c6f76e516cd4d6530f86fac53839fb0e54d1
Your Name <faizanshah123123123@gmail.com>
2021-04-23 19:58:50 +0100

    big improvements

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
-import ffmpeg
+import datetime
+import os
 
 class Downloader:
     def __init__(self):
@@ -9,7 +10,7 @@ class Downloader:
         self.youtube = YouTube(url)
 
     def get_videos(self):
-        return [stream for stream in self.youtube.streams.filter(only_video=True)]
+        return [stream for stream in self.youtube.streams.filter(only_video=True, video_codec="vp9")]
     
     def get_audios(self):
         return [stream for stream in self.youtube.streams.filter(only_audio=True)]
@@ -17,11 +18,58 @@ class Downloader:
     def get_audio_videos(self):
         return [stream for stream in self.youtube.streams.filter(progressive=True)]
     
-    def combine_audio_video(self, audio, video):
-        pass
-        #video_stream = ffmpeg.input(video.download().split("//")[-1])
-        #audio_stream = ffmpeg.input(audio.download().split("//")[-1])
-        #ffmpeg.output(audio_stream, video_stream, 'out.mp4').run()
-    
+    def find_video(self, resolution, fps):
+        video = None
 
-    #fix this
+        for stream in self.get_videos():
+            if str(stream.resolution) == resolution:
+                if str(stream.fps) == fps:
+                    video = stream
+                    break
+        
+        return video
+    
+    def find_audio(self, bitrate):
+        audio = None
+
+        for stream in self.get_audios():
+            print(bitrate)
+            print(stream.abr)
+            if str(stream.abr) == bitrate:
+                audio = stream
+                break
+        
+        print(audio)
+        return audio
+    
+    def find_audio_videos(self, resolution, fps, bitrate):
+        video = None
+
+        for stream in self.get_audio_videos():
+            if str(stream.resolution) == resolution:
+                if str(stream.fps) == fps:
+                    if str(stream.abr) == bitrate:
+                        video = stream
+                        break
+        
+        print(video)
+        return video
+    
+    def download(self, video):
+        print(video)
+        local_path = video.download()
+        fname = local_path.split("//")[-1]
+
+        return fname
+    
+    def get_title(self):
+        return self.youtube.title
+    
+    def get_thumbnail(self):
+        return self.youtube.thumbnail_url
+
+    def get_views(self):
+        return self.youtube.views
+
+    def get_duration(self):
+        return str(datetime.timedelta(seconds=self.youtube.length))
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 
 app = Flask(__name__)
 downloader = Downloader()
+app.secret_key = "37825789567878456784867878680"
 
 @app.context_processor
 def override_url_for():
@@ -23,19 +24,40 @@ def dated_url_for(endpoint, **values):
 @app.route("/", methods=["POST", "GET"])
 def home():
     if request.method == "POST":
-        url = request.form["ur"]
-        downloader.set_url(url)
-        videos = downloader.get_videos()
-        audios = downloader.get_audios()
-        audiovideos = downloader.get_audio_videos()
+        if "video_button" in request.form:
+            quality_list = request.form["video_button"].split(",")
+            video = downloader.find_video(quality_list[0], quality_list[1])
+            fname = downloader.download(video)
+            return send_file(fname, as_attachment=True)
+        elif "audio_button" in request.form:
+            video = downloader.find_audio(request.form["audio_button"])
+            fname = downloader.download(video)
+            return send_file(fname, as_attachment=True)
+        elif "audiovideo_button" in request.form:
+            quality_list = request.form["audiovideo_button"].split(",")
+            video = downloader.find_audio_videos(quality_list[0], quality_list[1], quality_list[2])
+            fname = downloader.download(video)
+            return send_file(fname, as_attachment=True)
+        else:
+            url = request.form["ur"]
+            session["url"] = url
+            downloader.set_url(session["url"])
+            videos = downloader.get_videos()
+            audios = downloader.get_audios()
+            audiovideos = downloader.get_audio_videos()
+            title = downloader.get_title()
+            thumbnail = downloader.get_thumbnail()
+            views = downloader.get_views()
+            duration = downloader.get_duration()
 
-        print(videos)
-        print(audios)
-        print(audiovideos)
+            print(duration)
 
-        return render_template("index.html")
+            return render_template("index.html", url=session["url"], videos=videos, audios=audios, audiovideos=audiovideos, title=title, thumbnail=thumbnail, views=views, duration=duration)
     else:
-        return render_template("index.html")
+        if "url" in session:
+            return render_template("index.html", url=session["url"])
+        else:
+            return render_template("index.html", url="")
         
 @app.route("/about")
 def about():
@@ -46,5 +68,5 @@ def contact():
     return "contact"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
 
